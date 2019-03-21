638e99ce0b366385cd6f46631081f9890507a292
Caio Ramos <caioramos97@gmail.com>
2019-03-20 23:09:27 -0300

    Usando hashrouter

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 
 import Index from './pages/Index'
 import More from './pages/More'
 
 export default () => (
-  <Router basename='~caiohsramos/mac0499'>
+  <Router>
     <Switch>
       <Route path='/more' exact component={More} />
       <Route path='*' component={Index} />
