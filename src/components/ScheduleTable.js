67f489b79e67d4e19286abb5c04bf03f2c14bf26
Caio Ramos <caioramos97@gmail.com>
2019-04-26 22:19:20 -0300

    Adicionando tabela e proposta

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -1,6 +1,55 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table'
 
+const tableContent = [{
+    date: 'Abr',
+    activity: 'Criação da página da disciplina',
+    status: 'Concluída'
+}, {
+    date: 'Abr',
+    activity: 'Ferramenta para análise automática usando o Sonar',
+    status: 'Concluída'
+}, {
+    date: 'Abr/Mai',
+    activity: 'Relacionar métricas de código com itens de dívida técnica',
+    status: 'Em andamento'
+}, {
+    date: 'Mai',
+    activity: 'Ajuste na ferramenta do Git',
+    status: 'Não realizada'
+}, {
+    date: 'Mai',
+    activity: 'Ajuste na ferramenta do JIRA',
+    status: 'Não realizada'
+}, {
+    date: 'Mai',
+    activity: 'Ajuste na ferramenta do Sonar',
+    status: 'Não realizada'
+}, {
+    date: 'Final de Jun',
+    activity: 'Escrita introdutória da monografia',
+    status: 'Não realizada'
+}, {
+    date: 'Jul/Ago',
+    activity: 'Método para priorizar se o item deve ou não ser pago',
+    status: 'Não realizada'
+}, {
+    date: 'Jul/Ago',
+    activity: 'Método para priorizar em categorias a importância',
+    status: 'Não realizada'
+}, {
+    date: 'Jul/Ago',
+    activity: 'Método para priorizar as dívidas em lista',
+    status: 'Não realizada'
+}, {
+    date: 'Começo de Out',
+    activity: 'Escrita final do trabalho',
+    status: 'Não realizada'
+}, {
+    date: 'Começo de Nov',
+    activity: 'Produção dos materiais auxiliares',
+    status: 'Não realizada'
+}]
 
 export default () => (
     <Table>
@@ -12,11 +61,13 @@ export default () => (
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Noop</td>
-                <td>Noop</td>
-                <td>Noop</td>
-            </tr>
+            {tableContent.map((row) => (
+                <tr>
+                    <td>{row.date}</td>
+                    <td>{row.activity}</td>
+                    <td>{row.status}</td>
+                </tr>
+            ))}
         </tbody>
     </Table>
 )
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -8,8 +8,13 @@ export default () => (
     <Grid>
         <Jumbo>
             <h1>TCC - Caio Henrique Silva Ramos</h1>
+            <h2>Proposta de trabalho</h2>
             <p>
-                **Proposta de estudo**
+                O objetivo do trabalho de formatura é construir uma ferramenta que auxilie na priorização de itens relativos a dívidas técnicas. Dívida técnica (ou débito técnico) é um conceito no desenvolvimento de software que representa os custos adicionais do retrabalho, que pode ser causado por decisões de implementação fáceis ao invés de outras que seriam melhores, porém demorariam mais.
+            </p>
+            <p>
+                O projeto irá focar em responder três questões acerca de um determinado débito técnico: pagar ou não pagar, categorizar sua importância e ordená-lo em relação aos outros débitos. Pretende-se fazer isso utilizando: métricas de código extraídas utilizando a ferramenta SonarQube, projeto open-source para analize contínua da qualidade de código; histórico de commits do Git, software de versionamento mais utilizado no mundo; e informações acerca do desenvolvimento e acompanhamento de tarefas e bugs com o JIRA. As informações serão consolidadas usando algoritmos de machine learning como KNN, CART, regressão linear e logística, SVN e redes neurais para responder as questões propostas no início.
+                A motivação por trás do desenvolvimento desse projeto é que não existe atualmente nenhuma aplicação que use todas as diversas informações disponíveis comentadas no paragrafo anterior para construir um relatório baseado não só no código fonte em si, mas no modelo de desenvolvimento de cada time.
             </p>
             <p>
                 <Link to='/schedule'>
